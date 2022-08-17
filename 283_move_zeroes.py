7fa0605cda373e8e0d26c548f7fc90c4abdfef88
Max <45366523+m3xw3ll@users.noreply.github.com>
2022-08-17 17:52:52 +0200

    Add 283_move_zeroes

--- 283_move_zeroes.py
+++ 283_move_zeroes.py
@@ -1,10 +1,10 @@
-nums = [0, 1, 0, 3, 12]
-j = 0
-for i in range(len(nums)):
-    print(i)
-    if nums[i] != 0:
-        nums[j], nums[i] = nums[i], nums[j]
-        j += 1
+def move_zeros(nums):
+    j = 0
+    for i in range(len(nums)):
+        if nums[i] != 0:
+            nums[j], nums[i] = nums[i], nums[j]
+            j += 1
+    print(nums)
 
 
-print(nums)
+move_zeros([0, 1, 0, 3, 12])
